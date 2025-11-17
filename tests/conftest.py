8091867f748ac969469f8a48dc2258e36e3e11d2
josephrich98 <josephrich98@gmail.com>
2025-11-16 16:42:08 -0800

    deleted conftest

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-# import nltk
-# import pytest
-
-# from word_counter.nltk_setup import ensure_nltk
-
-
-# @pytest.fixture(scope="session", autouse=True)
-# def ensure_nltk_test():
-#     ensure_nltk()
